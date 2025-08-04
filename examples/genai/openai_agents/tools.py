159630aaeab3498d4182898fe55b5f9785e9f9f9
Niels Bantilan <niels.bantilan@gmail.com>
2025-08-04 12:35:19 -0400

    implement drop-in replacement for function_tool
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- examples/genai/openai_agents/tools.py
+++ examples/genai/openai_agents/tools.py
@@ -36,7 +36,7 @@ env = flyte.TaskEnvironment(
     name="openai_agents_tools", resources=flyte.Resources(cpu=1, memory="250Mi"),
     image=(
         flyte.Image.from_debian_base(
-            name="openai_agents_tools",
+            name="openai_agents_tools_2",
             python_version=(3, 13),
         )
         .with_pip_packages(
@@ -55,30 +55,36 @@ env = flyte.TaskEnvironment(
 @function_tool
 # @flyte.trace
 @env.task
-async def get_weather(city: str) -> Weather:
+async def get_weather_tool(city: str) -> Weather:
     """Get the weather for a given city."""
     print("[debug] get_weather tool called")
+    print("tool context", flyte.ctx())
     return Weather(city=city, temperature_range="14-20C", conditions="Sunny with wind.")
 
 
 agent = Agent(
     name="Hello world",
     instructions="You are a helpful agent.",
-    tools=[get_weather],
+    tools=[get_weather_tool],
 )
 
 @env.task
-async def main() -> str:
-    result = await Runner.run(agent, input="What's the weather in Tokyo?")
-    print(result.final_output)
-    return str(result.final_output)
+async def run_agent():
+    # return await helper()
+    print("agent context", flyte.ctx())
+    out =  await get_weather_tool.on_invoke_tool(None, "{\"city\": \"Tokyo\"}")
+    # return out.model_dump_json()
+    # result = await Runner.run(agent, input="What's the weather in Tokyo?")
+    # result = await get_weather_tool("Tokyo")
+    # return result.model_dump_json()
+    # print(result.final_output)
+    # return str(result.final_output)
 
 
 if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
-    # flyte.init_from_config("../../../config.yaml")
-    # run = flyte.run(main)
-    # print(run.url)
-    # run.wait(run)
+    # import asyncio
+    # asyncio.run(run_agent())
+    flyte.init_from_config("../../../config.yaml")
+    run = flyte.run(run_agent)
+    print(run.url)
+    run.wait(run)
--- src/flyte/ai/openai/agents/_function_tools.py
+++ src/flyte/ai/openai/agents/_function_tools.py
@@ -1,23 +1,66 @@
 import typing
+import json
+from dataclasses import dataclass, asdict
+from functools import partial
 
-from flyte._task import AsyncFunctionTaskTemplate
+
+from flyte.models import NativeInterface
+from flyte._task import AsyncFunctionTaskTemplate, TaskTemplate
 from flyte._utils import lazy_module
 
 
 if typing.TYPE_CHECKING:
     import agents
-    from agents import function_tool as openai_function_tool, FunctionTool
+    from agents import function_tool as openai_function_tool, FunctionTool as OpenAIFunctionTool
+    from agents.function_schema import function_schema
+    from agents.tool_context import ToolContext
 else:
     agents = lazy_module("agents")
+    openai_function_tool = agents.function_tool
+    OpenAIFunctionTool = agents.FunctionTool
+    function_schema = lazy_module("agents.function_schema").function_schema
+    ToolContext = lazy_module("agents.tool_context").ToolContext
+
+
+@dataclass
+class FlyteFunctionTool(OpenAIFunctionTool):
+    task: TaskTemplate | None = None
+    native_interface: NativeInterface | None = None
+    report: bool = False
+
+    async def execute(self, *args, **kwargs):
+        return await self.task.execute(*args, **kwargs)
 
 
 def function_tool(
-    func: AsyncFunctionTaskTemplate,
-) -> agents.FunctionTool:
-    import ipdb; ipdb.set_trace()
-    return agents.FunctionTool(
-        name=func.name,
-        description=func.description,
-        params_json_schema=func.params_json_schema,
-        on_invoke_tool=func.on_invoke_tool,
-    )
+    func: AsyncFunctionTaskTemplate | typing.Callable | None = None,
+    **kwargs,
+) -> FlyteFunctionTool | OpenAIFunctionTool:
+    if func is None:
+        return partial(function_tool, **kwargs)
+
+    if isinstance(func, AsyncFunctionTaskTemplate):
+        async def _on_invoke_tool(ctx: ToolContext[typing.Any], input: str) -> typing.Any:
+            json_data: dict[str, typing.Any] = json.loads(input) if input else {}
+            schema = function_schema(func.func)
+            parsed = (
+                schema.params_pydantic_model(**json_data)
+                if json_data
+                else schema.params_pydantic_model()
+            )
+            args, kwargs_dict = schema.to_call_args(parsed)
+            out = await func(*args, **kwargs_dict)
+            return out
+        
+        _openai_fn_tool = asdict(openai_function_tool(func.func, **kwargs))
+        _openai_fn_tool.pop("on_invoke_tool")
+        return FlyteFunctionTool(
+            native_interface=func.native_interface,
+            task=func,
+            report=func.report,
+            on_invoke_tool=_on_invoke_tool,
+            **_openai_fn_tool
+
+        )
+
+    return openai_function_tool(func, **kwargs)
