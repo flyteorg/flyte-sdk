5d079f753646244d3646408656c2e46d1c80b5a5
Kevin Su <pingsutw@apache.org>
2025-08-20 11:36:48 -0700

    increase mem
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- examples/basics/vscode.py
+++ examples/basics/vscode.py
@@ -5,7 +5,7 @@ import flyte
 env = flyte.TaskEnvironment(
     name="hello_world",
     image=flyte.Image.from_debian_base(name="vscode"),
-    resources=flyte.Resources(cpu=1.5, memory="1000Mi"),
+    resources=flyte.Resources(cpu=1.5, memory="1500Mi"),
 )
 
 
@@ -22,7 +22,7 @@ async def say_hello_nested(data: str = "default string") -> str:
 
 if __name__ == "__main__":
     flyte.init_from_config("../../config.yaml", log_level=logging.DEBUG)
-    run = flyte.with_runcontext(env={"LOG_LEVEL": "10", "_F_E_VS": "True"}).run(
+    run = flyte.with_runcontext(env_vars={"LOG_LEVEL": "10", "_F_E_VS": "True"}).run(
         say_hello_nested, data="hello world", n=10
     )
     print(run.name)
