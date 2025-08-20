f9748e73b1ae8e10ff3e9132653d35c572481eb8
Kevin Su <pingsutw@apache.org>
2025-08-20 04:45:25 -0700

    fmt
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- src/flyte/_internal/controllers/remote/_action.py
+++ src/flyte/_internal/controllers/remote/_action.py
@@ -183,7 +183,7 @@ class Action:
         et.FromSeconds(int(end_time))
         et.nanos = int((end_time % 1) * 1e9)
 
-        spec = (
+        _ = (
             task_definition_pb2.TaskSpec(task_template=tasks_pb2.TaskTemplate(interface=typed_interface))
             if typed_interface
             else None
