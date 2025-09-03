03ea4cc7322303260c7b516d2312848b44629f61
Niels Bantilan <niels.bantilan@gmail.com>
2025-09-03 16:02:29 -0400

    add docstring
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- src/flyte/git/_config.py
+++ src/flyte/git/_config.py
@@ -2,9 +2,13 @@ import pathlib
 import subprocess
 
 
-def config_from_root() -> pathlib.Path:
+def config_from_root(path: pathlib.Path | str = ".flyte/config.yaml") -> pathlib.Path:
+    """Get the config file from the git root directory.
+
+    By default, the config file is expected to be in `.flyte/config.yaml` in the git root directory.
+    """
     result = subprocess.run(["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True)
     if result.returncode != 0:
         raise RuntimeError(f"Failed to get git root directory: {result.stderr}")
     root = pathlib.Path(result.stdout.strip())
-    return root / ".flyte" / "config.yaml"
+    return root / path
