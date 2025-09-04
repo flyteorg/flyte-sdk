af077f81a55986d16ee32ec61a9dae2470216263
Niels Bantilan <niels.bantilan@gmail.com>
2025-09-04 11:02:45 -0400

    Update default config path to ./.flyte/config.yaml, add `flyte.git` namespace to provide git utilities (#147)
    This PR:
    
    - Changes the default path of `flyte create config` to
    `./.flyte/config.yaml`
    - Adds a `flyte.git.config_from_root` utility function to get config
    files relative to a git project's root directory
    - Expands string config paths with `~` to the user's home directory.
    
    ---------
    
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- examples/basics/hello.py
+++ examples/basics/hello.py
@@ -24,7 +24,9 @@ def main(x_list: list[int]) -> float:
 
 
 if __name__ == "__main__":
-    flyte.init_from_config("../../config.yaml")  # establish remote connection from within your script.
+    import flyte.git
+
+    flyte.init_from_config(flyte.git.config_from_root())  # establish remote connection from within your script.
     run = flyte.run(main, x_list=list(range(10)))  # run remotely inline and pass data.
 
     # print various attributes of the run.
