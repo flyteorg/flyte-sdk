ef5698d5f07b73ac6e39e738eddf8c811650e70c
Kevin Su <pingsutw@apache.org>
2025-09-05 14:39:25 -0700

    Add integration tests
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ examples/integration_tests.py
@@ -0,0 +1,32 @@
+import os
+
+from basics.hello import main
+
+import flyte
+
+env = flyte.TaskEnvironment(name="integration_tests")
+
+
+@env.task
+async def integration_tests() -> None:
+    main([1, 2, 3])
+
+
+if __name__ == "__main__":
+    import flyte.git
+
+    flyte.init(
+        endpoint="dns:///playground.canary.unionai.cloud",
+        auth_type="ClientSecret",
+        client_id="flyte-sdk-ci",
+        client_credentials_secret=os.getenv("FLYTE_SDK_CI_TOKEN"),
+        insecure=False,
+        image_builder="remote",
+        project="flyte-sdk",
+        domain="development"
+    )
+    run = flyte.run(main, x_list=list(range(10)))
+
+    print(run.name)
+    print(run.url)
+    run.wait(run)
