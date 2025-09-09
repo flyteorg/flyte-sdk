ad559a5e4fb208f7c219ccf6fadb2e4d615da8ee
Ketan Umare <kumare3@users.noreply.github.com>
2025-09-08 22:13:34 -0700

    Nesting Asyncio
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

---
+++ examples/sync/async_in_sync.py
@@ -0,0 +1,30 @@
+import asyncio
+
+import nest_asyncio
+
+import flyte
+
+env = flyte.TaskEnvironment(
+    "asyncclient_sync",
+    image=flyte.Image.from_debian_base().with_pip_packages("aiohttp", "nest_asyncio"),
+)
+
+nest_asyncio.apply()
+
+
+async def async_in_sync() -> str:
+    await asyncio.sleep(1)
+    return "done"
+
+
+@env.task
+def call_async() -> str:
+    return asyncio.run(async_in_sync())
+
+
+if __name__ == "__main__":
+    import flyte.git
+    flyte.init_from_config(flyte.git.config_from_root())
+    run = flyte.run(call_async)
+    print(run.url)
+    run.wait()
