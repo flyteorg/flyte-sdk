1f3536551847f2f565f66e5de53fa0bb014fb4ac
Alex Wu <c.alexwu@gmail.com>
2025-09-12 05:21:51 +0800

    feature: add poetry layer to local image builder
    Signed-off-by: Alex Wu <c.alexwu@gmail.com>

---
+++ examples/deploy_patterns/poetryproject/poetryproject_test.py
@@ -0,0 +1,53 @@
+import asyncio
+import logging
+import pathlib
+from typing import List
+
+import flyte
+
+env = flyte.TaskEnvironment(
+    name="poetry",
+    resources=flyte.Resources(cpu=1, memory="1Gi"),
+    image=flyte.Image.from_debian_base().with_env_vars({"hello": "worldeeeeeeeeee"})
+    .with_poetry_project(
+        pyproject_file=pathlib.Path("pyproject.toml"),
+        pre=True,),
+)
+
+@env.task
+async def say_hello(data: str, lt: List[int]) -> str:
+    print(f"Hello, world! - {flyte.ctx().action}")
+    return f"Hello {data} {lt}"
+
+
+@env.task
+async def square(i: int = 3) -> int:
+    print(flyte.ctx().action)
+    return i * i
+
+
+@env.task
+async def say_hello_nested(data: str = "default string", n: int = 3) -> str:
+    print(f"Hello, nested! - {flyte.ctx().action}")
+    coros = []
+    for i in range(n):
+        coros.append(square(i=i))
+
+    vals = await asyncio.gather(*coros)
+    return await say_hello(data=data, lt=vals)
+
+
+if __name__ == "__main__":
+    flyte.init_from_config()
+    run = flyte.with_runcontext(
+        log_level=logging.DEBUG,
+        env_vars={"KEY": "V"},
+        labels={"Label1": "V1"},
+        annotations={"Ann": "ann"},
+        overwrite_cache=True,
+        interruptible=False,
+    ).run(say_hello_nested, data="hello world", n=10)
+    print(run.name)
+    print(run.url)
+    run.wait()
+
