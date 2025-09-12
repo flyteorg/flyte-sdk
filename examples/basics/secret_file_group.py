39df526306ef276ed787fe542a2938cfa3de9b47
fiedlerNr9 <jan@union.ai>
2025-09-12 12:15:25 -0700

    add example
    Signed-off-by: fiedlerNr9 <jan@union.ai>

---
+++ examples/basics/secret_file_group.py
@@ -0,0 +1,22 @@
+import flyte
+import pathlib
+
+GROUP = "arn:aws:secretsmanager:us-east-2:356633062068:secret"
+KEY = "V2_SECRET_TEST"
+SECRET_PATH = "/etc/flyte/secrets"
+
+env = flyte.TaskEnvironment(
+    "secret-fun",
+    secrets=flyte.Secret(group=GROUP, key=KEY, mount=pathlib.Path(SECRET_PATH)),
+)
+
+
+@env.task
+def main() -> str:
+    return pathlib.Path(f"{SECRET_PATH}/{GROUP}/{KEY.lower()}").read_text()
+
+
+if __name__ == "__main__":
+    flyte.init_from_config()
+    run = flyte.run(main)
+    print(run.url)
