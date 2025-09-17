e4ebec275df62dbe19815146bdd3e03ddf87c7eb
Kevin Su <pingsutw@apache.org>
2025-09-17 15:33:27 -0700

    Bigquery task example
    Signed-off-by: Kevin Su <pingsutw@apache.org>

---
+++ examples/connectors/bigquery_example.py
@@ -0,0 +1,25 @@
+import flyte
+from flyte.io import DataFrame
+from flyteplugins.connectors.bigquery.task import BigQueryConfig, BigQueryTask
+
+
+bigquery_env = flyte.TaskEnvironment(
+    name="bigquery_env",
+)
+
+
+bigquery_task = BigQueryTask(
+    name="bigquery",
+    inputs={"version": int},
+    output_dataFrame_type=DataFrame,
+    plugin_config=BigQueryConfig(ProjectID="flyte"),
+    query_template="SELECT * from dataset.flyte;",
+)
+
+bigquery_env.add_task(bigquery_task)
+
+
+if __name__ == "__main__":
+    flyte.init_from_config()
+    run = flyte.with_runcontext(mode="remote").run(bigquery_task, 123)
+    print(run.url)
---
+++ plugins/connectors/README.md
@@ -0,0 +1,9 @@
+# Union Connector Plugin
+
+TODO
+
+To install the plugin, run the following command:
+
+```bash
+pip install --pre flyteplugins-connectors
+```
