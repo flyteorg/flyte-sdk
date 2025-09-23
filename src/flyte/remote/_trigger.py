bfe8488612bc2a9c4677d3614eb056a1fb350a77
Ketan Umare <kumare3@users.noreply.github.com>
2025-09-23 09:51:45 -0700

    Fixed
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- src/flyte/remote/_trigger.py
+++ src/flyte/remote/_trigger.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Iterable
+from functools import cached_property
+from typing import AsyncIterator
 
 import flyte
 from flyte._initialize import ensure_client, get_client, get_common_config
@@ -10,6 +11,7 @@ from flyte._protos.workflow import task_definition_pb2, trigger_definition_pb2, 
 from flyte.syncify import syncify
 
 from ._common import ToJSONMixin
+from .._protos.common import identifier_pb2
 
 
 @dataclass
@@ -24,29 +26,70 @@ class TriggerDetails(ToJSONMixin):
         """
         ensure_client()
         cfg = get_common_config()
-        resp = await get_client().triggers_service.GetTriggerDetails(  # type: ignore
+        resp = await get_client().trigger_service.GetTriggerDetails(
             request=trigger_service_pb2.GetTriggerDetailsRequest(
-                name=name,
-                project=cfg.project,
-                domain=cfg.domain,
-                organization=cfg.org,
+                name=identifier_pb2.TriggerName(
+                    name=name,
+                    org=cfg.org,
+                    project=cfg.project,
+                    domain=cfg.domain,
+                ),
             )
         )
         return cls(pb2=resp.trigger)
+
+    @property
+    def name(self) -> str:
+        return self.id.name.name
+
+    @property
+    def id(self) -> identifier_pb2.TriggerIdentifier:
+        return self.pb2.id
+
+    @property
+    def task_id(self) -> task_definition_pb2.TaskIdentifier:
+        return self.pb2.spec.task_id
+
+    @property
+    def automation_spec(self) -> trigger_definition_pb2.TriggerAutomationSpec:
+        return self.pb2.automation_spec
+
+    @property
+    def meta(self) -> trigger_definition_pb2.TriggerMetadata:
+        return self.pb2.meta
+
+    @property
+    def status(self) -> trigger_definition_pb2.TriggerStatus:
+        return self.pb2.status
+
+    @property
+    def is_active(self) -> bool:
+        return self.pb2.spec.active
+
+    @cached_property
+    def trigger(self) -> trigger_definition_pb2.Trigger:
+        return trigger_definition_pb2.Trigger(
+            id=self.pb2.id,
+            task_id=self.pb2.spec.task_id,
+            automation_spec=self.pb2.automation_spec,
+            meta=self.pb2.meta,
+            status=self.pb2.status,
+            active=self.pb2.spec.active,
+        )
 
 
 @dataclass
 class Trigger(ToJSONMixin):
     pb2: trigger_definition_pb2.Trigger
-    details: trigger_definition_pb2.TriggerDetails | None = None
+    details: TriggerDetails | None = None
 
     @syncify
     @classmethod
     async def create(
-        cls,
-        trigger: flyte.Trigger,
-        task_name: str,
-        task_version: str | None = None,
+            cls,
+            trigger: flyte.Trigger,
+            task_name: str,
+            task_version: str | None = None,
     ) -> Trigger:
         """
         Create a new trigger in the Flyte platform.
@@ -83,22 +126,22 @@ class Trigger(ToJSONMixin):
         """
         Retrieve a trigger by its name and associated task name.
         """
-        ensure_client()
-        cfg = get_common_config()
-        resp = await get_client().triggers_service.GetTrigger(  # type: ignore
-            request=trigger_service_pb2.GetTriggerRequest(
-                name=name,
-                task_name=task_name,
-                project=cfg.project,
-                domain=cfg.domain,
-                organization=cfg.org,
-            )
+        details = TriggerDetails.get.aio(name=name)
+        return cls(
+            pb2=trigger_definition_pb2.Trigger(
+                id=details.id,
+                task_id=details.task_id,
+                automation_spec=details.details.automation_spec,
+                meta=details.meta,
+                status=details.status,
+                active=details.is_active,
+            ),
+            details=details,
         )
-        return cls(pb2=resp.trigger)
 
     @syncify
     @classmethod
-    async def listall(cls, task_name: str | None = None, limit: int = 100) -> Iterable[Trigger]:
+    async def listall(cls, task_name: str | None = None, limit: int = 100) -> AsyncIterator[Trigger]:
         """
         List all triggers associated with a specific task or all tasks if no task name is provided.
         """
@@ -106,62 +149,79 @@ class Trigger(ToJSONMixin):
         cfg = get_common_config()
         token = None
         while True:
-            resp = await get_client().triggers_service.ListTriggers(  # type: ignore
+            resp = await get_client().trigger_service.ListTriggers(
                 request=trigger_service_pb2.ListTriggersRequest(
+                    organization=cfg.org,
                     project=cfg.project,
                     domain=cfg.domain,
-                    organization=cfg.org,
-                    task_name=task_name,
-                    limit=limit,
                     token=token,
+                    limit=limit,
                 )
             )
             token = resp.token
-            for r in resp.secrets:
+            for r in resp.triggers:
                 yield cls(r)
             if not token:
                 break
 
     @syncify
     @classmethod
-    async def pause(cls, name: str, task_name: str):
+    async def deactivate(cls, name: str, task_name: str):
         """
         Pause a trigger by its name and associated task name.
         """
         ensure_client()
         cfg = get_common_config()
-        await get_client().triggers_service.UpdateTrigger(  # type: ignore
+        await get_client().trigger_service.UpdateTriggers(
             request=trigger_service_pb2.UpdateTriggersRequest(
-                name=name,
-                task_name=task_name,
+                organization=cfg.org,
                 project=cfg.project,
                 domain=cfg.domain,
-                organization=cfg.org,
+                name=name,
+                state=trigger_definition_pb2.TriggerState.TRIGGER_STATE_PAUSED,
             )
         )
 
     @syncify
     @classmethod
-    async def resume(cls, name: str, task_name: str, catchup: bool = False):
+    async def activate(cls, name: str, task_name: str):
         """
         Resume a paused trigger by its name and associated task name.
         """
         ensure_client()
         cfg = get_common_config()
-        await get_client().triggers_service.UpdateTrigger(  # type: ignore
+        await get_client().trigger_service.UpdateTriggers(
             request=trigger_service_pb2.UpdateTriggersRequest(
-                name=name,
-                task_name=task_name,
+                organization=cfg.org,
                 project=cfg.project,
                 domain=cfg.domain,
-                organization=cfg.org,
-                catchup=catchup,  # If True, it will run all missed executions since the last pause
+                name=name,
+                state=trigger_definition_pb2.TriggerState.TRIGGER_STATE_ACTIVE,
             )
         )
-        return
 
-    async def details(self) -> TriggerDetails:
-        pass
+    @syncify
+    @classmethod
+    async def delete(cls, name: str):
+        """
+        Delete a trigger by its name.
+        """
+        ensure_client()
+        cfg = get_common_config()
+        await get_client().trigger_service.DeleteTriggers(
+            request=trigger_service_pb2.DeleteTriggersRequest(
+                organization=cfg.org,
+                project=cfg.project,
+                domain=cfg.domain,
+                name=name,
+            )
+        )
+
+    async def get_details(self) -> TriggerDetails:
+        """
+        Get detailed information about this trigger.
+        """
+        return await TriggerDetails.get(name=self.pb2.name)
 
     def _rich_automation(self, automation: trigger_definition_pb2.CronAutomation):
         yield "cron", automation.expression
