cbca6d581734bb5cdac3d14ee4fd1521c069054e
machichima <nary12321@gmail.com>
2025-10-04 15:32:08 +0800

    feat: update example
    Signed-off-by: machichima <nary12321@gmail.com>

--- examples/image/image_from_name.py
+++ examples/image/image_from_name.py
@@ -8,42 +8,26 @@ logger.setLevel(logging.DEBUG)
 noop_env = flyte.TaskEnvironment(
     name="reuse_concurrency",
     resources=flyte.Resources(cpu=1, memory="1Gi"),
-    reusable=flyte.ReusePolicy(
-        replicas=2,
-        idle_ttl=60,
-        concurrency=100,
-        scaledown_ttl=60,
-    ),
-    image=flyte.Image.from_name("custom-image"),
+    image=flyte.Image.from_name("custom-image")
+    .with_apt_packages("ca-certificates")
+    .with_pip_packages("flyte", pre=True)
+    .with_local_v2(),
 )
 
 
 @noop_env.task
-async def main(x: int) -> int:
+async def main(x: int = 1) -> int:
     logger.debug(f"Task noop: {x}")
     return x
 
 
 if __name__ == "__main__":
-    from click.testing import CliRunner
-
-    from flyte.cli.main import main as cli_main
-
-    runner = CliRunner()
-
-    result = runner.invoke(
-        cli_main,
-        [
-            "run",
-            "--image",
-            # Assign image uri to name custom-image
-            "custom-image=ghcr.io/flyteorg/flyte",
-            "examples/image/image_from_name.py",
-            "main",
-            "--x",
-            "42",
-        ],
-    )
-
-    print("Exit code:", result.exit_code)
-    print("Output:", result.output)
+    # The image references can be set in config like following:
+    #
+    # image:
+    #   image_refs:
+    #     custom-image: debian:stable
+    flyte.init_from_config()
+    run = flyte.run(main)
+    print(run.name)
+    print(run.url)
