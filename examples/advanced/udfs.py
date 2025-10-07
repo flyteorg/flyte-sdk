7f6bb7b2f4969db8ea9d039eec479ed76cbd0df3
Ketan Umare <kumare3@users.noreply.github.com>
2025-10-06 18:28:42 -0700

    Write higher order function that work with udfs
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- examples/advanced/udfs.py
+++ examples/advanced/udfs.py
@@ -25,12 +25,8 @@ async def main() -> list[int]:
     results_coro_one = []
     results_coro_two = []
     for i in range(5):
-        results_coro_one.append(
-            asyncio.create_task(run_udf.override(short_name="run_udf_parent")(i, add_one_udf))
-        )
-        results_coro_two.append(
-            asyncio.create_task(run_udf.override(short_name="run_udf_inline")(i, fn_add_two_udf))
-        )
+        results_coro_one.append(asyncio.create_task(run_udf.override(short_name="run_udf_parent")(i, add_one_udf)))
+        results_coro_two.append(asyncio.create_task(run_udf.override(short_name="run_udf_inline")(i, fn_add_two_udf)))
 
     results_one = await asyncio.gather(*results_coro_one)
     results_two = await asyncio.gather(*results_coro_two)
