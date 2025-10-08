e74057a750f7f2da926d9e8576128ed17b7974d2
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-10-07 18:15:37 -0700

    use local again
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- examples/reuse/cancel_patterns.py
+++ examples/reuse/cancel_patterns.py
@@ -4,11 +4,11 @@ from pathlib import Path
 import flyte
 from flyte._image import PythonWheels
 
-# actor_dist_folder = Path("/Users/ytong/go/src/github.com/unionai/flyte/fasttask/worker-v2/dist")
-# wheel_layer = PythonWheels(wheel_dir=actor_dist_folder, package_name="unionai-reuse")
-# base = flyte.Image.from_debian_base()
-# actor_image = base.clone(addl_layer=wheel_layer)
-actor_image = flyte.Image.from_debian_base().with_pip_packages("unionai-reuse==0.1.7a0", index_url="https://test.pypi.org/simple/", pre=True)
+actor_dist_folder = Path("/Users/ytong/go/src/github.com/unionai/flyte/fasttask/worker-v2/dist")
+wheel_layer = PythonWheels(wheel_dir=actor_dist_folder, package_name="unionai-reuse")
+base = flyte.Image.from_debian_base()
+actor_image = base.clone(addl_layer=wheel_layer)
+# actor_image = flyte.Image.from_debian_base().with_pip_packages("unionai-reuse==0.1.7a0", index_url="https://test.pypi.org/simple/", pre=True)
 
 actor_env = flyte.TaskEnvironment(
     name="reuse_cancel_patterns",
