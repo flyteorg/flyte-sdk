e4a6a658ff8c6a7d9ad84620dbad0fa02f640254
Kevin Su <pingsutw@apache.org>
2025-10-10 12:34:39 -0700

    address comment
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- src/flyte/remote/_secret.py
+++ src/flyte/remote/_secret.py
@@ -31,7 +31,7 @@ class Secret(ToJSONMixin):
         else:
             secret_type = definition_pb2.SecretType.SECRET_TYPE_IMAGE_PULL_SECRET
             if project or domain:
-                raise ValueError("Project and domain should not be set when creating the image pull secret.")
+                raise ValueError(f"Project `{project}` or domain `{domain}` should not be set when creating the image pull secret.")
 
         if isinstance(value, str):
             secret = definition_pb2.SecretSpec(
