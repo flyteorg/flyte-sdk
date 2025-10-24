5bb85b99ee9c4a626f67c48c2f2bf934bf7b4037
Kevin Su <pingsutw@apache.org>
2025-10-24 13:34:39 -0700

    nit
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- examples/plugins/dask_example.py
+++ examples/plugins/dask_example.py
@@ -9,10 +9,7 @@ import flyte.remote
 import flyte.storage
 from flyte import Resources
 
-dask_plugin = (
-    "git+https://github.com/flyteorg/flyte-sdk.git@16c86432737757b2001d39e4dec8306d5c49e54a#subdirectory=plugins/dask"
-)
-image = flyte.Image.from_debian_base(python_version=(3, 12)).with_apt_packages("git").with_pip_packages(dask_plugin)
+image = flyte.Image.from_debian_base(python_version=(3, 12)).with_pip_packages("flyteplugins-dask")
 
 dask_config = Dask(
     scheduler=Scheduler(),
