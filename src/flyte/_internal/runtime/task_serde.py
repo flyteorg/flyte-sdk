20bb382f2716ce4ac8900e65395ab913bf9ae35a
Kevin Su <pingsutw@apache.org>
2025-10-24 12:10:48 -0700

    Improve the error message when the image is not found in the image cache
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- src/flyte/_internal/runtime/task_serde.py
+++ src/flyte/_internal/runtime/task_serde.py
@@ -221,10 +221,23 @@ def _get_urun_container(
         # This computes the image uri, computing hashes as necessary so can fail if done remotely.
         img_uri = task_template.image.uri
     elif serialize_context.image_cache and env_name not in serialize_context.image_cache.image_lookup:
-        img_uri = task_template.image.uri
-
-        logger.warning(
-            f"Image {task_template.image} not found in the image cache: {serialize_context.image_cache.image_lookup}."
+        raise flyte.errors.RuntimeUserError(
+            "MissingEnvironment",
+            f"Environment '{env_name}' not found in image cache.\n\n"
+            "💡 To fix this:\n"
+            "  1. If your parent environment calls a task in another environment, declare that dependency using 'depends_on=[...]'.\n"
+            "     Example:\n"
+            "         env1 = flyte.TaskEnvironment(\n"
+            "             name='outer',\n"
+            "             image=flyte.Image.from_debian_base().with_pip_packages('requests'),\n"
+            "             depends_on=[env2, env3],\n"
+            "         )\n"
+            "  2. If you're using os.getenv() to set the environment name, make sure the runtime environment has the same environment variable defined.\n"
+            "      Example:\n"
+            "          env = flyte.TaskEnvironment(\n"
+            "              name=os.getenv(\"my-name\"),\n"
+            "              env_vars={\"my-name\": os.getenv(\"my-name\")},\n"
+            "          )\n"
         )
     else:
         img_uri = serialize_context.image_cache.image_lookup[env_name]
