b5a29a9a7cdf431f6ed2f0fd99b33aec0260337a
Alex Wu <66259759+popojk@users.noreply.github.com>
2025-10-29 02:57:24 +0800

    Add addition attributes in RunSpec (#217)
    ## Changes
    1. Update `src/flyte/_run.py` to pass `service_account` and
    `raw_output_prefix` into RunSpec while creating a new run
    2. Update protos
    
    ---------
    
    Signed-off-by: Alex Wu <c.alexwu@gmail.com>

--- src/flyte/_run.py
+++ src/flyte/_run.py
@@ -134,7 +134,7 @@ class _Runner:
     async def _run_remote(self, obj: TaskTemplate[P, R, F] | LazyEntity, *args: P.args, **kwargs: P.kwargs) -> Run:
         import grpc
         from flyteidl2.common import identifier_pb2
-        from flyteidl2.core import literals_pb2
+        from flyteidl2.core import literals_pb2, security_pb2
         from flyteidl2.task import run_pb2
         from flyteidl2.workflow import run_definition_pb2, run_service_pb2
         from google.protobuf import wrappers_pb2
@@ -271,6 +271,14 @@ class _Runner:
             env_kv = run_pb2.Envs(values=kv_pairs)
             annotations = run_pb2.Annotations(values=self._annotations)
             labels = run_pb2.Labels(values=self._labels)
+            raw_data_storage = (
+                run_pb2.RawDataStorage(raw_data_prefix=self._raw_data_path) if self._raw_data_path else None
+            )
+            security_context = (
+                security_pb2.SecurityContext(run_as=security_pb2.Identity(k8s_service_account=self._service_account))
+                if self._service_account
+                else None
+            )
 
             try:
                 resp = await get_client().run_service.CreateRun(
@@ -288,6 +296,8 @@ class _Runner:
                             labels=labels,
                             envs=env_kv,
                             cluster=self._queue or task.queue,
+                            raw_data_storage=raw_data_storage,
+                            security_context=security_context,
                         ),
                     ),
                 )
@@ -620,8 +630,8 @@ def with_runcontext(
     :param interactive_mode: Optional, can be forced to True or False.
          If not provided, it will be set based on the current environment. For example Jupyter notebooks are considered
          interactive mode, while scripts are not. This is used to determine how the code bundle is created.
-    :param raw_data_path: Use this path to store the raw data for the run. Currently only supported for local runs,
-      and can be used to store raw data in specific locations. TODO coming soon for remote runs as well.
+    :param raw_data_path: Use this path to store the raw data for the run for local and remote, and can be used to
+         store raw data in specific locations.
     :param run_base_dir: Optional The base directory to use for the run. This is used to store the metadata for the run,
      that is passed between tasks.
     :param overwrite_cache: Optional If true, the cache will be overwritten for the run
