cfdd60283464f9b3cdf14de2544bde2f7b647a68
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-10-28 16:17:00 -0700

    example comments
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- examples/image/private_base_image.py
+++ examples/image/private_base_image.py
@@ -3,32 +3,18 @@ from flyte import Image
 
 """
 This example demonstrates how to build a new image using a private base image,
-then push the resulting image back to the same private registry.
 
-This workflow will change in the next couple weeks, but for now:
-
-First run the old CLI command to create a file. This will extract out from your local docker daemon logged in tokens.
-```
-union create imagepullsecret
-```
-
-
-Take the file that was created and then call the following to create a registry secret:
-```
-flyte create secret --type image_pull pingsutw --from-file <PATH/to/above file>
-```
 
 Keep in mind we only support one secret for now for a given image. If you have a private registry that you need to pull
 from, and a different private registry you want to push to, and hence need more than one registry secret, this is not
 supported.
 
-Note that you do not need a secret to push to (or pull from) the default remote builder registry. So in the example
-below, if you drop the `.clone(...)` call, you can also drop the `secrets` argument in the TaskEnvironment.
-
+Note that you do not need a secret to push to (or pull from) the default remote builder registry.
 """
+
 image = (
-    Image.from_base("pingsutw/private:d1742efed83fc4b18c7751e53e771bbe")
-    .clone(registry="docker.io/pingsutw", name="private", registry_secret="pingsutw")
+    Image.from_base("ghcr.io/wild-endeavor/newprivate:0977dcd202aef2c1262ded713fe12abd")
+    .clone(name="from-private-image", registry_secret="yt_ghcr_tst1")
     .with_apt_packages("vim")
     .with_local_v2()
 )
@@ -37,7 +23,7 @@ image = (
 Use the same registry secret in the task environment to ensure the container
 can pull the private image during task execution.
 """
-env = flyte.TaskEnvironment(name="private-image", image=image, secrets="pingsutw")
+env = flyte.TaskEnvironment(name="private-image", image=image)
 
 
 @env.task
