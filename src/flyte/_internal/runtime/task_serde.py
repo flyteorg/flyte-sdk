a18bb09262ae38c0a42e60f5d2cf3ac278676087
Ketan Umare <kumare3@users.noreply.github.com>
2025-11-17 14:38:52 -0800

    tests passing
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- src/flyte/_internal/runtime/task_serde.py
+++ src/flyte/_internal/runtime/task_serde.py
@@ -246,32 +246,7 @@ def _get_urun_container(
     if env_name is None:
         raise flyte.errors.RuntimeSystemError("BadConfig", f"Task {task_template.name} has no parent environment name")
 
-    if not serialize_context.image_cache:
-        # This computes the image uri, computing hashes as necessary so can fail if done remotely.
-        img_uri = task_template.image.uri if not isinstance(task_template.image, str) else task_template.image
-    elif serialize_context.image_cache and env_name not in serialize_context.image_cache.image_lookup:
-        raise flyte.errors.RuntimeUserError(
-            "MissingEnvironment",
-            f"Environment '{env_name}' not found in image cache.\n\n"
-            "💡 To fix this:\n"
-            "  1. If your parent environment calls a task in another environment,"
-            " declare that dependency using 'depends_on=[...]'.\n"
-            "     Example:\n"
-            "         env1 = flyte.TaskEnvironment(\n"
-            "             name='outer',\n"
-            "             image=flyte.Image.from_debian_base().with_pip_packages('requests'),\n"
-            "             depends_on=[env2, env3],\n"
-            "         )\n"
-            "  2. If you're using os.getenv() to set the environment name,"
-            " make sure the runtime environment has the same environment variable defined.\n"
-            "     Example:\n"
-            "         env = flyte.TaskEnvironment(\n"
-            '             name=os.getenv("my-name"),\n'
-            '             env_vars={"my-name": os.getenv("my-name")},\n'
-            "         )\n",
-        )
-    else:
-        img_uri = serialize_context.image_cache.image_lookup[env_name]
+    img_uri = lookup_image_in_cache(serialize_context, env_name, img)
 
     return tasks_pb2.Container(
         image=img_uri,
