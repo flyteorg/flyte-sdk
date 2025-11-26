398a5534919532897bd5984e6f9ce3282ccf7b95
Kevin Su <pingsutw@apache.org>
2025-11-26 13:26:19 -0800

    Clean up
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- examples/connectors/databricks_example.py
+++ examples/connectors/databricks_example.py
@@ -13,15 +13,9 @@ image = (
     .with_apt_packages("git", "vim")
     .with_env_vars({"UV_PYTHON": "/databricks/python3/bin/python"})
     .with_pip_packages(
-        "git+https://github.com/flyteorg/flyte-sdk.git@62a1cf3bf4ba091beff2804263664e083038dd6c#subdirectory=plugins/connectors",
+        "flyteplugins-connectors[databricks]",
         pre=True,
     )
-    .with_pip_packages(
-        "git+https://github.com/flyteorg/flyte-sdk.git@62a1cf3bf4ba091beff2804263664e083038dd6c#subdirectory=plugins/spark",
-        pre=True,
-    )
-    .with_local_v2()
-    .with_pip_packages("nest-asyncio", "aiohttp", "click==8.1.6")
     .with_env_vars({"AWS_REGION": "us-west-1", "LOG_LEVEL": 10})
 )
 
@@ -42,24 +36,24 @@ databricks_conf = Databricks(
     executor_path="/databricks/python3/bin/python",
     databricks_conf={
         "run_name": "flytekit databricks plugin example",
-        # "new_cluster": {
-        #     "spark_version": "13.3.x-scala2.12",
-        #     "autoscale": {
-        #         "min_workers": 1,
-        #         "max_workers": 1,
-        #     },
-        #     "node_type_id": "m6i.large",
-        #     "num_workers": 1,
-        #     "aws_attributes": {
-        #         "availability": "SPOT_WITH_FALLBACK",
-        #         "instance_profile_arn": "arn:aws:iam::339713193121:instance-profile/databricks-demo",
-        #         "ebs_volume_type": "GENERAL_PURPOSE_SSD",
-        #         "ebs_volume_count": 1,
-        #         "ebs_volume_size": 100,
-        #         "first_on_demand": 1,
-        #     },
-        # },
-        "existing_cluster_id": "1113-204018-tb9vr2fm",
+        "new_cluster": {
+            "spark_version": "13.3.x-scala2.12",
+            "autoscale": {
+                "min_workers": 1,
+                "max_workers": 1,
+            },
+            "node_type_id": "m6i.large",
+            "num_workers": 1,
+            "aws_attributes": {
+                "availability": "SPOT_WITH_FALLBACK",
+                "instance_profile_arn": "arn:aws:iam::339713193121:instance-profile/databricks-demo",
+                "ebs_volume_type": "GENERAL_PURPOSE_SSD",
+                "ebs_volume_count": 1,
+                "ebs_volume_size": 100,
+                "first_on_demand": 1,
+            },
+        },
+        # "existing_cluster_id": "1113-204018-tb9vr2fm",  # you can use an existing cluster by uncommenting this line
         "timeout_seconds": 3600,
         "max_retries": 1,
     },
@@ -92,26 +86,17 @@ async def hello_databricks_nested() -> float:
     n = 1 * partitions
     spark = flyte.ctx().data["spark_session"]
     count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("getttttt pi")
     res = get_pi(count, partitions)
     print(f"result: {res}")
     return res
 
 
-async def test_main():
-    import flyte.storage as storage
-
-    proto_str = b"".join(
-        [c async for c in storage.get_stream(path="s3://my-v2-connector/a209f3771e880f2ea8eb44a24ca572cf/inputs.pb")]
-    )
-    print(proto_str)
-
-
 if __name__ == "__main__":
     flyte.init_from_config(storage=S3().auto())
-    run = flyte.with_runcontext(mode="local", raw_data_path="s3://my-v2-connector/").run(hello_databricks_nested)
+    run = flyte.with_runcontext(mode="local", raw_data_path="s3://my-connector/").run(hello_databricks_nested)
     print(run)
+
+    # Remote execution
+    # run = flyte.with_runcontext(mode="remote").run(hello_databricks_nested)
     # print("run name:", run.name)
     # print("run urlllll:", run.url)
-
-    # asyncio.run(test_main())
