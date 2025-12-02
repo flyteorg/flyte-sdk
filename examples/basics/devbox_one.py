82e50182b0d2f0fdd9334b685fcc33946181cac8
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-12-02 00:09:13 -0800

    add some older generated files
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- examples/basics/devbox_one.py
+++ examples/basics/devbox_one.py
@@ -12,12 +12,12 @@ import flyte
 controller_dist_folder = Path("/Users/ytong/go/src/github.com/flyteorg/flyte-sdk/rs_controller/dist")
 wheel_layer = PythonWheels(wheel_dir=controller_dist_folder, package_name="flyte_controller_base")
 base = flyte.Image.from_debian_base()
-actor_image = base.clone(addl_layer=wheel_layer)
+rs_controller_image = base.clone(addl_layer=wheel_layer)
 
 env = flyte.TaskEnvironment(
     name="hello_world",
     resources=flyte.Resources(cpu=1, memory="1Gi"),
-    image=actor_image,
+    image=rs_controller_image,
 )
 
 
