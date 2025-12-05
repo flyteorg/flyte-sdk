a9fe3dfa219f3a6a1953d1e40955c31b4c2aa792
Kevin Su <pingsutw@apache.org>
2025-12-04 17:04:20 -0800

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- src/flyte/_internal/imagebuild/utils.py
+++ src/flyte/_internal/imagebuild/utils.py
@@ -22,7 +22,7 @@ def copy_files_to_context(src: Path, context_path: Path, ignore_patterns: list[s
     :param context_path: The context path where the files should be copied to
     """
     if src.is_absolute() or ".." in str(src):
-        rel_path = PurePath(*src.parts[1:]).as_posix()
+        rel_path = PurePath(*src.parts[1:])
         dst_path = context_path / "_flyte_abs_context" / rel_path
     else:
         dst_path = context_path / src
@@ -33,7 +33,7 @@ def copy_files_to_context(src: Path, context_path: Path, ignore_patterns: list[s
         shutil.copytree(src, dst_path, dirs_exist_ok=True, ignore=shutil.ignore_patterns(*ignore_patterns))
     else:
         shutil.copy(src, dst_path)
-    return dst_path
+    return dst_path.as_posix()
 
 
 def get_and_list_dockerignore(image: Image) -> List[str]:
