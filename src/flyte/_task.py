22ceef19622be09cc944595f83c002b44aab509c
Kevin Su <pingsutw@apache.org>
2025-12-11 13:42:52 -0800

    updated example
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- src/flyte/_task.py
+++ src/flyte/_task.py
@@ -358,6 +358,7 @@ class TaskTemplate(Generic[P, R, F]):
         pod_template: Optional[Union[str, PodTemplate]] = None,
         queue: Optional[str] = None,
         interruptible: Optional[bool] = None,
+        link: Optional[Link] = None,
         **kwargs: Any,
     ) -> TaskTemplate:
         """
@@ -376,6 +377,8 @@ class TaskTemplate(Generic[P, R, F]):
          passed directly to the task.
         :param pod_template: Optional override for the pod template to use for the task.
         :param queue: Optional override for the queue to use for the task.
+        :param interruptible: Optional override for the interruptible policy for the task.
+        :param link: Optional override for the Link associated with the task.
         :param kwargs: Additional keyword arguments for further overrides. Some fields like name, image, docs,
          and interface cannot be overridden.
 
@@ -440,6 +443,7 @@ class TaskTemplate(Generic[P, R, F]):
             pod_template=pod_template,
             interruptible=interruptible,
             queue=queue or self.queue,
+            link=link or self.link,
             **kwargs,
         )
 
