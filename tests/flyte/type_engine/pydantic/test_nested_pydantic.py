d69a942fb635a7fe7ad94655268e1921fe0ede94
Ketan Umare <kumare3@users.noreply.github.com>
2025-12-14 13:46:56 -0800

    Fixed
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- tests/flyte/type_engine/pydantic/test_nested_pydantic.py
+++ tests/flyte/type_engine/pydantic/test_nested_pydantic.py
@@ -15,6 +15,21 @@ class MyInput(BaseModel):
 
 
 @pytest.mark.asyncio
+async def test_simple_pydantic():
+    input = MyNestedModel(x=1)
+    lit = TypeEngine.to_literal_type(MyNestedModel)
+    lv = TypeEngine.to_literal(input, MyNestedModel, lit)
+
+    assert lit
+    assert lv
+
+    guessed = TypeEngine.guess_python_type(lit)
+    assert guessed
+    v = guessed(x=1)
+    assert v.__dict__ == input.model_dump()
+
+
+@pytest.mark.asyncio
 async def test_nested_pydantic():
     input = MyInput(x=1, y=2, m=MyNestedModel(x=1))
     lit = TypeEngine.to_literal_type(MyInput)
