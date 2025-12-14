9de4eba8f9dc7321ef86b5f0265afdd5986ef4b6
Ketan Umare <kumare3@users.noreply.github.com>
2025-12-14 15:33:48 -0800

    more tests
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- examples/reftask_complex_io/pydantic_io.py
+++ examples/reftask_complex_io/pydantic_io.py
@@ -7,6 +7,15 @@ import flyte
 env = flyte.TaskEnvironment(name="py-io")
 
 
+class LeafModel(BaseModel):
+    j: int
+
+
+class MyNestedModel2(BaseModel):
+    y: int
+    m: LeafModel
+
+
 class MyNestedModel(BaseModel):
     x: int
 
@@ -15,12 +24,15 @@ class MyInput(BaseModel):
     x: int
     y: int
     m: MyNestedModel
+    m2: MyNestedModel2
 
 
 @env.task
 async def dynamic_wf(
     workflow_config: MyInput,  # Pydantic BaseModel
     user_params: dict[str, Any],
+    x: int,
+    y: str,
 ) -> None:
     print(workflow_config, flush=True)
     print(user_params, flush=True)
@@ -34,11 +46,22 @@ async def main():
     import flyte.remote
 
     ref_task = flyte.remote.Task.get("py-io.dynamic_wf", auto_version="latest")
-    await ref_task(workflow_config={"x": 1, "y": 2, "m": {"x": 1}}, user_params={"y": dynamic_wf})
+    await ref_task(
+        workflow_config={"x": 1, "y": 2, "m": {"x": 1}, "m2": {"y": 1, "m": {"j": 1}}},
+        user_params={"y": dynamic_wf},
+        x=1,
+        y="Hello",
+    )
 
 
 def run_direct():
-    r = flyte.run(dynamic_wf, MyInput(x=1, y=2, m=MyNestedModel(x=1)), {"x": dynamic_wf})
+    r = flyte.run(
+        dynamic_wf,
+        MyInput(x=1, y=2, m=MyNestedModel(x=1), m2=MyNestedModel2(y=1, m=LeafModel(j=1))),
+        {"x": dynamic_wf},
+        x=1,
+        y="Hello",
+    )
     print(r.url)
 
 
@@ -46,8 +69,14 @@ def run_ref():
     import flyte.remote
 
     flyte.deploy(env)
+    data_dict = {
+        "workflow_config": {"x": 1, "y": 2, "m": {"x": 1}, "m2": {"y": 1, "m": {"j": 1}}},
+        "user_params": {"y": dynamic_wf},
+        "x": 1,
+        "y": "Hello",
+    }
     ref_task = flyte.remote.Task.get("py-io.dynamic_wf", auto_version="latest")
-    r = flyte.run(ref_task, workflow_config={"x": 1, "y": 2, "m": {"x": 1}}, user_params={"y": dynamic_wf})
+    r = flyte.run(ref_task, **data_dict)
     print(r.url)
 
 
