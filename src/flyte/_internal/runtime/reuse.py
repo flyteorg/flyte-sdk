c833d507c5c3e1c27871b7a124c6f27f533f7fc7
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-12-18 15:45:53 -0800

    lint
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- src/flyte/_internal/runtime/reuse.py
+++ src/flyte/_internal/runtime/reuse.py
@@ -53,7 +53,7 @@ def extract_unique_id_and_image(
     else:
         components += f":{reuse_policy.replicas}"
     if reuse_policy.idle_ttl:
-        components += f":{reuse_policy.idle_ttl.total_seconds()}"
+        components += f":{reuse_policy.idle_ttl.total_seconds()}"  # type: ignore [union-attr]
     if reuse_policy.get_scaledown_ttl() is not None:
         components += f":{reuse_policy.get_scaledown_ttl()}"
     if code_bundle is not None:
@@ -114,7 +114,7 @@ def add_reusable(
             "parallelism": reuse_policy.concurrency,
             "min_replica_count": reuse_policy.min_replicas,
             "replica_count": reuse_policy.max_replicas,
-            "ttl_seconds": reuse_policy.idle_ttl.total_seconds() if reuse_policy.idle_ttl else None,
+            "ttl_seconds": reuse_policy.idle_ttl.total_seconds() if reuse_policy.idle_ttl else None,  # type: ignore [union-attr]
             "scaledown_ttl_seconds": scaledown_ttl.total_seconds() if scaledown_ttl else None,
         },
     }
