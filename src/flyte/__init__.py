204b052659b58c88eb5d02849e10decfaeb675d0
Ketan Umare <kumare3@users.noreply.github.com>
2025-12-26 14:27:03 -0800

    Do not import prefetch at flyte level
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- src/flyte/__init__.py
+++ src/flyte/__init__.py
@@ -6,7 +6,6 @@ from __future__ import annotations
 
 import sys
 
-from . import prefetch
 from ._build import build
 from ._cache import Cache, CachePolicy, CacheRequest
 from ._context import ctx
@@ -106,7 +105,6 @@ __all__ = [
     "init_in_cluster",
     "logger",
     "map",
-    "prefetch",
     "run",
     "serve",
     "trace",
