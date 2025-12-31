1798b80bcfc6f9f3d2bdbb69a8e37fe5ff125d73
Yee Hing Tong <wild-endeavor@users.noreply.github.com>
2025-12-31 14:53:39 -0800

    clean dist and clean up some create controller comments
    Signed-off-by: Yee Hing Tong <wild-endeavor@users.noreply.github.com>

--- src/flyte/_bin/runtime.py
+++ src/flyte/_bin/runtime.py
@@ -136,6 +136,7 @@ def main(
     # Use Rust controller if env var is set, otherwise default to Python controller
     use_rust = os.getenv(_F_USE_RUST_CONTROLLER, "").lower() in ("1", "true", "yes")
     controller_type = "rust" if use_rust else "remote"
+    print(f"In runtime: controller kwargs are: {controller_kwargs}")
     controller = create_controller(ct=controller_type, **controller_kwargs)
 
     ic = ImageCache.from_transport(image_cache) if image_cache else None
