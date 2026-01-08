a72f5e9c81eeade3177a811c31c4252b9df4f212
debasisdwivedy <dwivedy.debasis@gmail.com>
2026-01-08 22:44:22 +0530

    adding click check for config file path and type as file object
    Signed-off-by: debasisdwivedy <dwivedy.debasis@gmail.com>

--- src/flyte/cli/main.py
+++ src/flyte/cli/main.py
@@ -125,7 +125,7 @@ def _verbosity_to_loglevel(verbosity: int) -> int | None:
     "--config",
     "config_file",
     required=False,
-    type=click.Path(exists=True,file_okay=True),
+    type=click.Path(exists=True,dir_okay=False),
     help="Path to the configuration file to use. If not specified, the default configuration file is used.",
 )
 @click.option(
