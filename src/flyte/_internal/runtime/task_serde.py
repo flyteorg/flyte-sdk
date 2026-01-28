8a91a8f5e6c779782d1c39c55c3522263a323be8
Kevin Su <pingsutw@apache.org>
2026-01-27 23:40:46 -0800

    fix
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- src/flyte/_internal/runtime/task_serde.py
+++ src/flyte/_internal/runtime/task_serde.py
@@ -128,14 +128,12 @@ def get_proto_task(
     extra_config: typing.Dict[str, str] = {}
     pod = None
     container = None
-    sql = None
+    sql = task.sql(serialize_context)
 
     if task.pod_template and not isinstance(task.pod_template, str):
         pod = _get_k8s_pod(_get_urun_container(serialize_context, task), task.pod_template)
         extra_config[_PRIMARY_CONTAINER_NAME_FIELD] = task.pod_template.primary_container_name
-    elif task.sql is not None:
-        sql = task.sql(serialize_context)
-    else:
+    elif sql is None:
         container = _get_urun_container(serialize_context, task)
 
     log_links = []
