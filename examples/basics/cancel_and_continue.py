01f7e7a91091c0e66efc13e05e67bd4158e6b59c
John Votta <jpvotta@gmail.com>
2026-01-29 10:40:39 -0800

    Add example of a workflow whose actions can be canceled from the UI and still succeed
    Signed-off-by: John Votta <jpvotta@gmail.com>

---
+++ examples/basics/cancel_and_continue.py
@@ -0,0 +1,31 @@
+import asyncio
+import time
+import flyte
+
+
+env = flyte.TaskEnvironment("env")
+
+@env.task
+async def maybe_take_a_while(x: int) -> int:
+    time.sleep(x)
+    return x * x
+
+@env.task
+async def fanout_calculation(ls: list[int]) -> int:
+    coros = []
+    with flyte.group("fanout-with-long-action"):
+        for i in ls:
+            coros.append(maybe_take_a_while(i))
+        results = await asyncio.gather(*coros, return_exceptions=True)
+        for i, res in enumerate(results):
+            if isinstance(res, Exception):
+                print(f"Task {i} failed with {type(res).__name__}: {res}")
+            else:
+                print(f"Task {i} succeeded with result: {res}")
+    return sum(r for r in results if not isinstance(r, Exception))
+
+
+if __name__ == "__main__":
+    flyte.init_from_config()
+    r = flyte.run(fanout_calculation, [1, 2, 3, 10, 100])
+    print(r.url)
