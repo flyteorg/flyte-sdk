dbe9af3b2f3e70409a9adc4fb5f1a9fe33218312
Kevin Su <pingsutw@apache.org>
2026-02-02 22:59:48 -0800

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- examples/basics/hello.py
+++ examples/basics/hello.py
@@ -21,7 +21,7 @@ def main(x_list: list[int]) -> float:
     if x_len < 10:
         raise ValueError(f"x_list doesn't have a larger enough sample size, found: {x_len}")
 
-    y_list = list(flyte.map(fn, x_list))  # flyte.map is like Python map, but runs in parallel.
+    y_list = list(flyte.map(fn, x_list))            # flyte.map is like Python map, but runs in parallel.
     y_mean = sum(y_list) / len(y_list)
     return y_mean
 
