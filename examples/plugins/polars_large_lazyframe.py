7362ccd41ea16a50adf7e011dd0480af0681458d
Niels Bantilan <niels.bantilan@gmail.com>
2026-02-05 20:23:09 -0500

    lint
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- examples/plugins/polars_large_lazyframe.py
+++ examples/plugins/polars_large_lazyframe.py
@@ -1,19 +1,16 @@
 from typing import Any
 
-
+import polars as pl
 from polars.lazyframe.frame import LazyFrame
 
-
 import flyte
-import polars as pl
-
 
 env = flyte.TaskEnvironment(
     name="polars",
     resources=flyte.Resources(memory="1Gi"),
-    image=flyte.Image.from_debian_base(
-        install_flyte=True, flyte_version="2.0.0b52"
-    ).with_pip_packages("flyteplugins-polars", pre=True),
+    image=flyte.Image.from_debian_base(install_flyte=True, flyte_version="2.0.0b52").with_pip_packages(
+        "flyteplugins-polars", pre=True
+    ),
 )
 
 
@@ -24,12 +21,14 @@ async def get_big_lazyframe() -> pl.LazyFrame:
     N_REPEATS = 5_000  # total rows = N_ROWS * N_REPEATS
     STR_SIZE = 32  # bytes per string (approx)
 
-    lf = pl.DataFrame({
-        "id": pl.arange(0, N_ROWS, eager=True),
-        "value": (pl.arange(0, N_ROWS, eager=True) * 0.1),
-        "payload_a": pl.select(pl.lit("x" * STR_SIZE).repeat_by(N_ROWS).flatten()),
-        "payload_b": pl.select(pl.lit("y" * STR_SIZE).repeat_by(N_ROWS).flatten()),
-    }).lazy()
+    lf = pl.DataFrame(
+        {
+            "id": pl.arange(0, N_ROWS, eager=True),
+            "value": (pl.arange(0, N_ROWS, eager=True) * 0.1),
+            "payload_a": pl.select(pl.lit("x" * STR_SIZE).repeat_by(N_ROWS).flatten()),
+            "payload_b": pl.select(pl.lit("y" * STR_SIZE).repeat_by(N_ROWS).flatten()),
+        }
+    ).lazy()
 
     return pl.concat([lf] * N_REPEATS)
 
@@ -46,9 +45,7 @@ async def preprocess_data(lf: pl.LazyFrame) -> str:
 async def main() -> str:
     print("polars fun")
     lf = await get_big_lazyframe[Any, Any, LazyFrame]()
-    return await preprocess_data(
-        lf=lf
-    )
+    return await preprocess_data(lf=lf)
 
 
 if __name__ == "__main__":
