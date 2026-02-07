d748f8d03832be2096eb2120b5cab3841d95e4df
Ketan Umare <kumare3@users.noreply.github.com>
2026-02-06 16:57:59 -0800

    Better docs
    Signed-off-by: Ketan Umare <kumare3@users.noreply.github.com>

--- src/flyte/cli/_start.py
+++ src/flyte/cli/_start.py
@@ -8,7 +8,18 @@ def start():
 
 @start.command()
 def tui():
-    """Launch TUI explore mode to browse past local runs."""
+    """
+    Launch TUI explore mode to browse past local runs. To use the TUI install `pip install flyte[tui]`
+    TUI, allows you to explore all your local runs if you have persistence enabled.
+
+    Persistence can be enabled in 2 ways,
+    1. By setting it in the config to record every local run
+    ```bash
+    flyte create config --endpoint ...  --local-persistence
+    ```
+    2. By passing it in flyte.init(local_persistence=True)
+    This will record all `flyte.run` runs, that are local and are within the flyte.init being active.
+    """
     from flyte.cli._tui import launch_tui_explore
 
     launch_tui_explore()
