6cf254a85ca6db4b4fa7c5cf7a0da5fe66e58e02
Kevin Su <pingsutw@apache.org>
2026-02-25 21:36:57 -0800

    update examples
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- examples/project_structures/uv_workspace/albatross/packages/bird_feeder/src/bird_feeder/actions.py
+++ examples/project_structures/uv_workspace/albatross/packages/bird_feeder/src/bird_feeder/actions.py
@@ -4,12 +4,12 @@ from seeds.utils import get_default_seed_name
 
 import flyte
 
-UV_PROJECT_ROOT = Path(__file__).parent.parent.parent  # bird_feeder
+UV_WORKSPACE_ROOT = Path(__file__).parent.parent.parent.parent.parent
 
 bird_env = flyte.TaskEnvironment(
     name="bird_feeder",
     image=flyte.Image.from_debian_base().with_uv_project(
-        pyproject_file=(UV_PROJECT_ROOT / "pyproject.toml"),
+        pyproject_file=(UV_WORKSPACE_ROOT / "pyproject.toml"),
     ),
 )
 
@@ -18,9 +18,3 @@ bird_env = flyte.TaskEnvironment(
 def get_feeder():
     print("Get bird feeder")
     print(f"Default seed: {get_default_seed_name()}")
-
-
-if __name__ == '__main__':
-    flyte.init_from_config(root_dir=UV_PROJECT_ROOT.parent)
-    run = flyte.run(get_feeder)
-    print(run.url)
