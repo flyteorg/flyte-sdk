bb91f188cb20f44e236b96d86f1d5b8a26d467dd
Niels Bantilan <niels.bantilan@gmail.com>
2026-03-19 13:00:54 -0400

    updates
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- plugins/hitl/src/flyteplugins/hitl/_event.py
+++ plugins/hitl/src/flyteplugins/hitl/_event.py
@@ -43,7 +43,7 @@ event_image = (
     flyte.Image.from_debian_base(flyte_version="2.0.9", name="hitl-event-app-image")
     .with_pip_packages("fastapi", "uvicorn", "python-multipart", "aiofiles")
     .with_apt_packages("git")
-    .with_pip_packages("git+https://github.com/flyteorg/flyte-sdk.git@38843b6")
+    .with_pip_packages("git+https://github.com/flyteorg/flyte-sdk.git@b0732aff")
     # .with_pip_packages("flyteplugins-hitl==2.0.9")
 )
 
@@ -151,6 +151,7 @@ class Event(Generic[T]):
     async def _serve_app(cls) -> flyte.AppHandle:
         """Serve the app and return the app handle."""
         await flyte.init_in_cluster.aio()
+        print("Building image manually...")
         image_build = await flyte.build.aio(event_image, force=True, wait=True)
         print("Image built: ", image_build.uri)
         event_app_env.image = image_build.uri
