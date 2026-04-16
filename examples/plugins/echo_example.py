d1a2d9f5836404a1e23a634a6e5e74a3d7f20fac
pmahindrakar-oss <prafulla.mahindrakar@gmail.com>
2026-04-15 22:10:14 -0700

    Add echo plugin support in the sdk
    Signed-off-by: pmahindrakar-oss <prafulla.mahindrakar@gmail.com>

---
+++ examples/plugins/echo_example.py
@@ -0,0 +1,26 @@
+import pathlib
+
+import flyte
+from flyteplugins.echo import Echo
+
+# The echo plugin runs in leaseworker, not inside this task image.
+# Use any normal image your environment can resolve.
+image = flyte.Image.from_debian_base(python_version=(3, 12))
+
+echo_env = flyte.TaskEnvironment(
+    name="echo-env",
+    image=image,
+    plugin_config=Echo(),
+)
+
+
+@echo_env.task
+async def echo_identity(message: str) -> str:
+    return message
+
+
+if __name__ == "__main__":
+    flyte.init_from_config("/Users/praful/temp/v2/config_dogfood.yaml")
+    run = flyte.run(echo_identity, message="hello")
+    print("run name:", run.name)
+    print("run url:", run.url)
